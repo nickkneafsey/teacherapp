e8d20f1a896b765546c53a0e621ab617eb52c31a
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-05-07 15:15:02 -0700

    Photo upload working on backend

--- server/controllers/students_controller.js
+++ server/controllers/students_controller.js
@@ -1,13 +1,43 @@
 const studentsModel = require('../models/students_model');
+const Jimp = require('jimp');
+const AWS = require('aws-sdk');
+const config = require('../../config');
+AWS.config.update({accessKeyId: config.ACCESS_KEY_ID, secretAccessKey: config.SECRET_ACCESS_KEY, region: 'us-east-1'});
+
+const s3 = new AWS.S3();
 
 module.exports = {
   getAll: function(req, res) {
-    params = [];
+    const params = [];
     studentsModel.getAll(params, function (err, results) {
       if (err) {
         res.sendStatus(500);
       }
       res.json(results);
     });
+  },
+
+  addPhotoUrl: function(req, res) {
+    const fileName = req.file.originalname.replace(/ /gi, '+') //+Math.floor(Math.random()*5000)
+    const url = 'https://s3.amazonaws.com/' + config.BUCKET_NAME + '/' + fileName;
+
+
+    const params = {
+      Bucket: config.BUCKET_NAME,
+      Key: fileName,
+      ACL: 'public-read',
+      ContentType: req.file.mimetype,
+      Body: req.file.buffer
+    };
+    console.log(params)
+    s3.putObject(params, function(err, data) {
+      console.log('DATA', data)
+      if (err) {
+        res.send(500);
+      }
+
+      res.send(url)
+    })
+
   }
 };
--- server/routes/students_router.js
+++ server/routes/students_router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const studentsController = require('../controllers/students_controller')
 
 const router = express.Router();
 
 router.get('/', studentsController.getAll);
+router.post('/', multer().single('studentPhoto'), studentsController.addPhotoUrl)
 
 module.exports = router;
